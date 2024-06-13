9a1b69b464f56d2b7c38723287e627faf6dc541b
Binuda Kalugalage <bjithmalk@gmail.com>
2024-06-14 01:34:35 +1000

    Create board, added game logic from ur-cli

--- src/window.py
+++ src/window.py
@@ -17,14 +17,47 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from gi.repository import Adw
-from gi.repository import Gtk
+from gi.repository import Gtk, Adw
 
 @Gtk.Template(resource_path='/com/github/binudakal/gnomeur/window.ui')
 class GnomeUrWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'GnomeUrWindow'
 
-    label = Gtk.Template.Child()
+    leftTile1 = Gtk.Template.Child()
+    leftTile2 = Gtk.Template.Child()
+    leftTile3 = Gtk.Template.Child()
+    leftTile4 = Gtk.Template.Child()
+    rightTile1 = Gtk.Template.Child()
+    rightTile2 = Gtk.Template.Child()
+    rightTile3 = Gtk.Template.Child()
+    rightTile4 = Gtk.Template.Child()
+    commonTile5 = Gtk.Template.Child()
+    commonTile6 = Gtk.Template.Child()
+    commonTile7 = Gtk.Template.Child()
+    commonTile8 = Gtk.Template.Child()
+    commonTile9 = Gtk.Template.Child()
+    commonTile10 = Gtk.Template.Child()
+    commonTile11 = Gtk.Template.Child()
+    commonTile12 = Gtk.Template.Child()
+    leftTile13 = Gtk.Template.Child()
+    leftTile14 = Gtk.Template.Child()
+    rightTile13 = Gtk.Template.Child()
+    rightTile14 = Gtk.Template.Child()
+
+    # enableButtons = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        # self.enableButtons.connect("clicked", self.on_clicked)
+
+    def set_sensitivity(self, button_id, sensitivity):
+        """Set the sensitivity of a button by its ID."""
+        button = getattr(self, f'{button_id}')
+
+        if button:
+            button.set_sensitive(sensitivity)
+
+
+    # def on_clicked(self, button):
+    #     self.set_sensitivity("commonTile7", True)
+
